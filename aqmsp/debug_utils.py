aaf78f35cecc5a651c35db130856ed4aeab1d10e
patel-zeel <patel_zeel@iitgn.ac.in>
2023-09-07 16:14:51 +0530

    checkpoint

---
+++ aqmsp/debug_utils.py
@@ -0,0 +1,18 @@
+import os
+
+
+def set_verbose(verbose: bool):
+    assert isinstance(verbose, bool), "verbose must be a boolean"
+    os.environ["AQMSP_VERBOSE"] = str(verbose)
+
+
+def get_verbose():
+    verbose = os.environ.get("AQMSP_VERBOSE")
+    if verbose is None:
+        return False
+    elif verbose == "True":
+        return True
+    elif verbose == "False":
+        return False
+    else:
+        raise ValueError("AQMSP_VERBOSE must be either True or False")
