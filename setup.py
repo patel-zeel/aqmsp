1669c32d397bbe9d09cf7c5a60388d6705312908
patel-zeel <patel_zeel@iitgn.ac.in>
2023-09-23 15:31:48 +0530

    try to embed aqmsp_data for easy install

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
-    requirements = f.read().strip().split('\n')
+with open("requirements.txt") as f:
+    requirements = f.read().strip().split("\n")
 
 setup(
     packages=find_packages(),
     python_requires=">=3.6",
     install_requires=requirements,
+    extras_require={
+        "submodules": [
+            "aqmsp_data",
+        ],
+    },
     include_package_data=True,
 )
